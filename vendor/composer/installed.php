88ae56e87fc1e8959efb375d9d4929bf7247d125
ashfaq867 <mitf19e032@gmail.com>
2024-07-28 22:59:58 +0500

    .

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '489488d46f7004a731dc06a8edba5301a648bd75',
+        'reference' => 'edf59802ea698717b7dcf50e98ac066116ef9299',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '489488d46f7004a731dc06a8edba5301a648bd75',
+            'reference' => 'edf59802ea698717b7dcf50e98ac066116ef9299',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
